061e9dda8d6239954ed57cd5df083ababc6b3c34
Scott Klein <scotty.klei@gmail.com>
2020-12-28 18:46:59 +1100

    Implemenitng time selection in order

--- LocalPizza.Core/Orders/Order.cs
+++ LocalPizza.Core/Orders/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NodaTime;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +12,35 @@ namespace LocalPizza.Core.Orders
         public int Id { get; set; }
         public string Name { get; set; }
         public Address DeliveryAddress { get; set; }
-        public DateTime RequestDelivery { get; set; }
+        public LocalTime RequestDelivery { get; set; }
         public List<ItemOrder> OrderItems { get; set; }
         public OrderStatus Status { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime DeliveredTime { get; set; }
+        public LocalDateTime Created { get; set; }
+        public LocalDateTime DeliveredTime { get; set; }
         public string DeliveryInstructions { get; set; }
+
+        public List<LocalTime> ListTimes()
+        {
+            var now = new LocalTime(DateTime.Now.Hour, DateTime.Now.Minute);
+            LocalTime first = new LocalTime();
+            if (now.Minute / 15 == 3)
+            {
+                first = first.PlusHours(1);
+            } else
+            {
+                first = first.PlusMinutes(15 + (now.Minute / 15));
+            }
+            //make this a constant somewhere meaningful.
+            LocalTime closeTime = new LocalTime(22,30); //Represents the closing time of the store.
+
+            List<LocalTime> times = new List<LocalTime>();
+            times.Add(first);
+            while (first < closeTime)
+            {
+                first = first.PlusMinutes(15);
+            }
+
+            return times;
+        }
     }
 }
