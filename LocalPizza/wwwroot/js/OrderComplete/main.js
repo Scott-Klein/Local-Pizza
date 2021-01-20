edec7b873eabbd5d9f2cfa1153b4d2bd3a64454b
Scott Klein <scotty.klei@gmail.com>
2021-01-20 12:12:04 +1100

    I think everything is finished.

--- LocalPizza/wwwroot/js/OrderComplete/main.js
+++ LocalPizza/wwwroot/js/OrderComplete/main.js
@@ -5,9 +5,9 @@ const OrderView = {
         <div id="grid-container">
             <div>
                 <order-tracker :status="this.order.status" @clear-data="this.clearData()"></order-tracker>
-                <order-data></order-data>
+                <order-data :order="this.order"></order-data>
             </div>
-            <order-items></order-items>
+            <order-items :order="this.order"></order-items>
 
         </div>
         `,
@@ -84,21 +84,34 @@ app.component('order-tracker',
                     default:
                         break;
                 }
+            },
+            dot() {
+                let offset = 40;
+                return offset + this.status * 40;
             }
         },
         template:
             /*html*/
             `
             <div id="order-tracker" class="order-pane">
-                <h2>Order Tracker</h2>
-                <svg class="circle-chart" viewbox="0 0 180 180" width="180" height="180" xmlns="http://www.w3.org/2000/svg">
-                    <circle :class="this.circleClass" stroke="#ffffff" stroke-width="6"  stroke-linecap="round" fill="none" cx="90" cy="90" r="85" />
-                    <circle :class="this.circleClass" stroke="#fff0" stroke-width="6"  stroke-linecap="round" fill="#ffffff3a" cx="90" cy="90" r="55" />
-                    <text class="large-svg-text" x="50" y ="102" fill="white">60</text>
-                    <text class="small-svg-text" x="58" y ="127" fill="white">Minutes</text>
-                </svg>
+                <div id="order-circle">
+                    <h2>Order Tracker</h2>
+                    <svg class="circle-chart" viewbox="0 0 180 180" width="180" height="180" xmlns="http://www.w3.org/2000/svg">
+                        <circle :class="this.circleClass" stroke="#ffffff" stroke-width="6"  stroke-linecap="round" fill="none" cx="90" cy="90" r="85" />
+                        <circle :class="this.circleClass" stroke="#fff0" stroke-width="6"  stroke-linecap="round" fill="#ffffff3a" cx="90" cy="90" r="55" />
+                        <text class="large-svg-text" x="56" y ="98" fill="white">60</text>
+                        <text class="small-svg-text" x="58" y ="123" fill="white">Minutes</text>
 
-                <p>{{this.StatusEnum[this.status]}}</p>
+                    </svg>
+                    <svg height="30" width="200">
+                        <circle :cx="this.dot" cy="15" r="9" stroke="#fff0" stroke-width="2" fill="#fff"/>
+                        <circle cx="40" cy="15" r="9" stroke="#fff0" stroke-width="2" fill="#fff4"/>
+                        <circle cx="80" cy="15" r="9" stroke="#fff0" stroke-width="2" fill="#fff4"/>
+                        <circle cx="120" cy="15" r="9" stroke="#fff0" stroke-width="2" fill="#fff4"/>
+                        <circle cx="160" cy="15" r="9" stroke="#fff0" stroke-width="2" fill="#fff4"/>
+                    </svg>
+                    <p id="status-indicator">{{this.StatusEnum[this.status]}}</p>
+                </div>
             </div>
             `,
         data() {
@@ -115,21 +128,36 @@ app.component('order-tracker',
 )
 
 app.component('order-data', {
+    props: {
+        order: Object
+    },
     template:
         /*html*/
         `
         <div id="order-data" class="order-pane">
             <h2>Delivery details</h2>
+            <p>Requested Delivery Time: {{order.requestDeliveryTime}}</p>
+            <p>Delivery Instructions:<br>{{order.deliveryInstructions}}</p>
+            <p>Name: {{order.name}}</p>
+            <p>Phone: {{order.phone}}</p>
         </div>
         `
 })
 
 app.component('order-items', {
+    props: {
+        order: Object
+    },
     template:
     /*html*/
         `
-        <div class="order-pane">
-            <h2>Order</h2>
+        <div id="order-items" class="order-pane">
+            <h2>Your Order</h2>
+            <div class="order-grid" v-for="item in order.orderItems">
+                <p>{{item.item.name}}</p>
+                <p>Quantity : {{item.quantity}}</p>
+                <p> $ {{item.quantity * item.item.price}}</p>
+            </div>
         </div>
         `
 })
